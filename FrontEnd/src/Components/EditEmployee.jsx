fea07951989285c8c6b4604b024139985028a86c
Harshith-121q <harshithkumarlingampally35@gmail.com>
2026-05-18 14:29:08 +0530

    Update EditEmployee.jsx

--- FrontEnd/src/Components/EditEmployee.jsx
+++ FrontEnd/src/Components/EditEmployee.jsx
@@ -34,7 +34,7 @@ export default function EditEmployee() {
             setLoading(true)
             setError("")
             let res = await axios.put(
-                `https://employee-proj-ten.vercel.app//emp-api/employees/${state._id}`,
+                `https://employee-proj-ten.vercel.app/emp-api/employees/${state._id}`,
                 modifiedEmpObj
             )
 
